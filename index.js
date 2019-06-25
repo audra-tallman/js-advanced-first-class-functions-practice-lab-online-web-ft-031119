5de894f545adbd1d7a9df08d3a25cb2d2f03a846
Audra Tallman <audi.tallman@gmail.com>
2019-06-25 00:53:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ const driversByName = function(drivers) {
   });
 };
 
+const totalRevenue = function(drivers) {
+  return drivers.reduce(function(total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
